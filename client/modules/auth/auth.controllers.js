39e13d56b4ee50f0bc2de865fb6fd7cc1a28469a
Tyler <tyhlewis@gmail.com>
2014-09-29 11:30:33 -0700

    (feat) adds basic functionality to auth page

--- client/modules/auth/auth.controllers.js
+++ client/modules/auth/auth.controllers.js
@@ -1,5 +1,24 @@
 angular.module('checkmate')
 
-.controller('AuthController', ['$scope', '$state', function($scope, $state) {
-  //
+.controller('AuthController', ['$scope', '$state', 'Auth', function($scope, $state, Auth) {
+
+  $scope.signUpDisplay = Auth.context;
+
+  $scope.toggleView = function() {
+    Auth.context = !Auth.context;
+    $scope.signUpDisplay = Auth.context;
+  };
+  
+  $scope.newUser = {};
+
+  $scope.existingUser = {};
+
+  $scope.signUpUser = function() {
+    Auth.signUpUser($scope.newUser);
+  };
+
+  $scope.logInUser = function() {
+    Auth.logInUser($scope.existingUser);
+  };
+
 }]);
--- client/modules/auth/auth.spec.js
+++ client/modules/auth/auth.spec.js
@@ -20,4 +20,26 @@ describe('AuthController', function() {
     expect(ctrl).to.not.equal(undefined);
   });
 
+  it('sign up fields should be stored in a newUser object', function() {
+    expect(scope.newUser).to.be.a('object');
+  });
+
+  it('log in fields should be stored in an existingUser object', function() {
+    expect(scope.existingUser).to.be.a('object');
+  });
+
+  it('should toggle views between sign up and log in', function() {
+    scope.signUpDisplay = true;
+    scope.toggleView();
+    expect(scope.signUpDisplay).to.equal(false);
+  });
+
+  it('should pass user input sign up data to Auth service for authentication', function() {
+    expect(scope.signUpUser).to.be.a('function');
+  });
+
+  it('should pass user input log in data to Auth service for authentication', function() {
+    expect(scope.logInUser).to.be.a('function');
+  });
+
 });
