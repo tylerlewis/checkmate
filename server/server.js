a02c41f0584d8d906b8190267cfa426bae7b4e41
Tyler <tyhlewis@gmail.com>
2014-09-29 09:45:43 -0700

    (feat) adds initial setup for passport auth

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('./auth.js');
 var app = express();
 var port = process.env.PORT || 8000;
 
@@ -7,3 +8,10 @@ app.use(express.static(__dirname + '../../client'));
 app.listen(port, function() {
   console.log('Now listening to ', port);
 });
+
+app.post('/login', 
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }));
