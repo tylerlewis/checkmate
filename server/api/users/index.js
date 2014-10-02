4472fc72621a5eed3c85956e0bcb12ced4b6c310
Tyler <tyhlewis@gmail.com>
2014-10-02 10:29:31 -0700

    (feat) adds mongoose orm, adds initial connection between api and db

---
+++ server/api/users/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var controller = require('./users.controller.js');
+
+var router = express.Router();
+
+router.post('/signup', controller.signupUser);
+router.post('/login', controller.loginUser);
+
+module.exports = router;
