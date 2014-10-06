61eb1b412db2507aeaf383ea36c3d4aa76f9d54a
Tyler <tyhlewis@gmail.com>
2014-10-06 02:37:06 -0700

    (feat) adds basic functionality to group page

--- client/modules/group/group.controllers.js
+++ client/modules/group/group.controllers.js
@@ -1,15 +1,30 @@
 angular.module('checkmate')
 
-.controller('GroupController', ['$scope', '$state', function($scope, $state) {
+.controller('GroupController', ['$scope', '$state', 'Group', function($scope, $state, Group) {
   
-  $scope.groupMembers = [];
+  $scope.groupMembers = Group.userSeed[0].users;
 
-  $scope.bills = [];
+  $scope.bills = Group.groupSeed[0].bills;
+
+  $scope.report = Group.splitBills();
+
+  $scope.showAddBillForm = false;
+
+  $scope.addBillFormDisplay = function() {
+    $scope.showAddBillForm = !$scope.showAddBillForm;
+  };
 
   $scope.newBill = {};
 
   $scope.addBill = function() {
-
+    $scope.addBillFormDisplay();
+    var rand = Math.floor((Math.random() * 3) + 0);
+    $scope.newBill.whoPaid = Group.userSeed[0].users[rand].name;
+    var amount = parseInt($scope.newBill.amount, 10);
+    $scope.newBill.amount = amount;
+    Group.addBill($scope.newBill);
+    $scope.report = Group.splitBills();
+    $scope.newBill = {};
   };
 
 }]);
