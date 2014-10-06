af5d2b84fc2726f7954b5fa3a51d5630ee74e112
Tyler <tyhlewis@gmail.com>
2014-10-05 22:07:40 -0700

    (feat) adds basic structure to html and controller files

--- client/modules/group/group.controllers.js
+++ client/modules/group/group.controllers.js
@@ -1,5 +1,15 @@
 angular.module('checkmate')
 
 .controller('GroupController', ['$scope', '$state', function($scope, $state) {
-  //
+  
+  $scope.groupMembers = [];
+
+  $scope.bills = [];
+
+  $scope.newBill = {};
+
+  $scope.addBill = function() {
+
+  };
+
 }]);
