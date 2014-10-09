018da333a16991cbec4e42770041dea4fbe9c5a1
Tyler <tyhlewis@gmail.com>
2014-10-08 22:45:02 -0700

    (feat) adds schema to MySQL database

--- server/db/checkmate.sql
+++ server/db/checkmate.sql
@@ -1,4 +1,43 @@
-CREATE DATABASE checkmate;
+CREATE DATABASE Checkmate;
 
-USE checkmate;
+USE Checkmate;
 
+CREATE TABLE Users (
+  UserID int NOT NULL AUTO_INCREMENT,
+  FirstName varchar(255),
+  LastName varchar(255),
+  Username varchar(255),
+  Password varchar(255),
+  PRIMARY KEY (UserID)
+);
+
+CREATE TABLE Groups (
+  GroupID int NOT NULL AUTO_INCREMENT,
+  Name varchar(255),
+  Password varchar(255),
+  UserID int,
+  PRIMARY KEY (GroupID),
+  FOREIGN KEY (UserID) REFERENCES Users (UserID)
+);
+
+CREATE TABLE Bills (
+  BillID int NOT NULL AUTO_INCREMENT,
+  WhoPaid varchar(255),
+  Type varchar(255),
+  Amount decimal(65, 2),
+  Created date,
+  GroupID int,
+  UserID int,
+  PRIMARY KEY (BillID),
+  FOREIGN KEY (GroupID) REFERENCES Groups (GroupID),
+  FOREIGN KEY (UserID) REFERENCES Users (UserID)
+);
+
+CREATE TABLE Link (
+  LinkID int NOT NULL AUTO_INCREMENT,
+  GroupID int,
+  UserID int,
+  PRIMARY KEY (LinkID),
+  FOREIGN KEY (GroupID) REFERENCES Groups (UserID),
+  FOREIGN KEY (UserID) REFERENCES Users (UserID)
+);
