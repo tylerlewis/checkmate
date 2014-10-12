236ddb5bc5ce23ce73a535afa453126015e1a5a4
Tyler <tyhlewis@gmail.com>
2014-10-11 20:49:55 -0700

    (feat) adds sql commands for finding and creating groups

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var bcrypt = require('bcrypt');
 
 var dbConnection = mysql.createConnection({
   host: 'localhost',
@@ -10,24 +11,32 @@ var dbConnection = mysql.createConnection({
 dbConnection.connect();
 
 module.exports.saveUser = function(user, callback) {
-  executeQuery('INSERT INTO Users(Username, Password) VALUES(?, ?);', [user.username, user.password], callback);
+  executeQuery('INSERT INTO Users(username, password) VALUES(?, ?);', [user.username, user.password], callback);
 };
 
 module.exports.findUser = function(user, callback) {
-  console.log(user.username)
-  executeQuery('SELECT * FROM Users WHERE Username = ? LIMIT 1;', [user.username], callback);
+  executeQuery('SELECT * FROM Users WHERE username = ? LIMIT 1;', [user.username], callback);
 };
 
 module.exports.createGroup = function(group, callback) {
-
+  module.exports.findUser({ username: group.user }, function(err, user) {
+    group.password = hashPassword(group.password);
+    executeQuery('INSERT INTO Groups(name, password, userId) VALUES(?, ?, ?);', [group.name, group.password, user[0].userId], callback);
+  });
 };
 
-module.exports.updateGroup = function(group, callback) {
-
+module.exports.joinGroup = function(group, password, callback) {
+  module.exports.findUser({ username: group.user }, function(err, user) {
+    if(validatePassword(group.password, password)) {
+      group.password = hashPassword(group.password);
+      executeQuery('INSERT INTO Groups(name, password, userId) VALUES(?, ?, ?);', [group.name, group.password, user[0].userId], callback);
+    }
+    else { callback('Password incorrect.'); }
+  });
 };
 
 module.exports.findGroup = function(group, callback) {
-
+  executeQuery('SELECT * FROM Groups WHERE name = ? LIMIT 1;', [group.name], callback);
 };
 
 var executeQuery = function(query, param, callback){
@@ -41,4 +50,12 @@ var executeQuery = function(query, param, callback){
       callback(err, results);
     });
   }
+};
+
+var validatePassword = function(password, storedPassword) {
+  return bcrypt.compareSync(password, storedPassword); 
+};
+
+var hashPassword = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
 };
