b87a6859429679ed9910b1901f32e00824fe99eb
Tyler <tyhlewis@gmail.com>
2014-10-12 20:10:57 -0700

    (feat) adds routing for groups and bills

--- server/routes.js
+++ server/routes.js
@@ -4,8 +4,8 @@ var db = require('./db/db.js');
 
 module.exports = function(app, passport) {
 
-  app.use('/api/users', require('./api/users'));
-  app.use('/api/groups', require('./api/groups'));
+  // app.use('/api/users', require('./api/users'));
+  // app.use('/api/groups', require('./api/groups'));
 
   app.post('/auth/signup', passport.authenticate('local-signup'), function(request, response) {
     response.send(200);
@@ -14,6 +14,11 @@ module.exports = function(app, passport) {
   app.post('/auth/login', passport.authenticate('local-login'), function(request, response) {
     console.log(request, response)
     response.send(200);
+  });
+
+  app.get('/auth/logout', function(request, response) {
+    request.logout();
+    response.redirect('/');
   });
 
   app.post('/groups/create', function(request, response) {
@@ -35,16 +40,19 @@ module.exports = function(app, passport) {
       else if(!group.length) { response.send(401, 'Group name or password is incorrect.'); }
       else {
         db.joinGroup(request.body, group[0].password, function(err, group) {
-          if(err) { response.send(err); }
+          if(err) { response.status(401).send('Password incorrect.'); }
           else { response.send(group); }
         });
       }
     });
   });
 
-  app.get('/auth/logout', function(request, response) {
-    request.logout();
-    response.redirect('/');
+  app.get('/groups', function(request, response) {
+    console.log('Group request', request)
+  });
+
+  app.post('/bills', function(request, response) {
+    console.log('Bill request', request)
   });
   
 };
